62a8dacea26f13d7e5d48ab93c426c04b4540994
unknown <t01kas11@.uoa.abdn.ac.uk>
2012-05-07 18:30:05 +0100

    working??

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,4 +4,11 @@ class TopicsController < ApplicationController
       @topic = @forum.topics.create(params[:topic])
       redirect_to forum_path(@forum)
     end
+    
+    def destroy
+      @forum = Forum.find(params[:forum_id])
+      @topic = @forum.topics.find(params[:id])
+      @topic.destroy
+      redirect_to forum_path(@forum)
+    end
 end
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,5 +1,5 @@
 class Forum < ActiveRecord::Base
     validates :name,  :presence => true
     validates :title, :presence => true, :length => { :minimum => 4 }
-    has_many :topics
+    has_many :topics, :dependent => :destroy
 end
