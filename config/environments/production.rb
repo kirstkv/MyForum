3d07aaadbc6f663c5b706b6d84f7ea49348fa8c6
unknown <t01kas11@.uoa.abdn.ac.uk>
2012-05-11 02:52:08 +0100

    slowly heroku will die

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Forumapp::Application.configure do
   config.assets.precompile += %w( widgEditor.css g ) 
   config.assets.precompile += %w( gmap4rails.css  ) 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
